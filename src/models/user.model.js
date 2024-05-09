c2178574f3df2fbd1d2596c6459c2c897471a36d
mohamedx2 <hamroun1969@gmail.com>
2024-05-09 22:23:47 +0100

    Initial commit

---
+++ src/models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+// Declare the Schema of the Mongo model
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mobile: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    dateNais: {
+        type: Date,
+        required: true
+    },
+    image: {
+        type: String, // Assuming the image will be stored as a URL
+        required: false // Set to true if image is mandatory
+    }
+});
+
+//Export the model
+module.exports = mongoose.model('User', userSchema);
