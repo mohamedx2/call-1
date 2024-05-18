578937c21112ef94d96fda6c49f385ca572ee1ac
mohamedx2 <hamroun1969@gmail.com>
2024-05-18 17:34:02 +0100

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,38 +1,42 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require('mongoose');
 
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        index:true,
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        index: true,
     },
-    lastName:{
-        type:String,
-        required:true,
-        index:true,
+    lastName: {
+        type: String,
+        required: true,
+        index: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    mobile:{
-        type:String,
-        required:true,
+    mobile: {
+        type: String,
+        required: true,
     },
-    username:{
-        type:String,
-        required:true,
-        unique:true
+    username: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
     },
-    dateNais:{
-        type:Date,
-        required:true
+    dateNais: {
+        type: Date,
+        required: true
+    },
+    image: {
+        type: String, // Assuming the image will be stored as a URL
+        required: false // Set to true if image is mandatory
     }
 });
 
